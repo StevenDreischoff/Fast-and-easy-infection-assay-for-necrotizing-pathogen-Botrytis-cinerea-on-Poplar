bfbe2629ad045893d5252376c5ea4a0022b6d81b
StevenDreischoff <110172879+StevenDreischoff@users.noreply.github.com>
2022-07-28 14:06:26 +0200

    Add files via upload

---
+++ pliman example script.R
@@ -0,0 +1,34 @@
+#fill XXXXXX according to your files 
+
+setwd("C:/XXXXXX")
+library(pliman)
+
+healthy <- image_import("healthy.jpg") 
+#load reference for healthy
+symptoms <- image_import("symp.jpg") 
+#load reference for symptoms
+background <- image_import("background.jpg") 
+#load reference for background
+
+res <-measure_disease(pattern ="XXXXXX", 
+                      #name of your file sequence
+                      img_healthy = healthy, 
+                      #reference for healthy
+                      img_symptoms = symptoms, 
+                      #reference for symptoms
+                      img_background = background, 
+                      #reference for background
+                      parallel = FALSE,
+                      #computes more than one picture at a time DOES NOT WORK YET
+                      fill_hull = FALSE,
+                      #does not automatically filles holes
+                      dir_original = "C:/XXXXXX", 
+                      #folder of your files
+                      save_image = TRUE, 
+                      #saves the processed pictures
+                      dir_processed = "C:/XXXXXX/processed") 
+#folder for processed pictures
+
+write.csv(res[["severity"]],"C:/XXXXXX/processed/sympare.csv") 
+#saves the results als .csv
+
